7c9973fb7bd27649f337f1be2e8d9fa0ba228edb
phatshibukai <vutienphat0809@gmail.com>
2023-07-21 07:43:52 +0700

    feat: intial commit

---
+++ git test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git_test
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
